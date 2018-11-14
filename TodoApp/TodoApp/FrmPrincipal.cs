7ef1faa410257a2f7f93e184a9b9762455b8e967
EduardoRM <eduarRM12@hotmail.com>
2018-11-14 13:45:28 -0500

    Cambios de rama dev

--- TodoApp/TodoApp/FrmPrincipal.cs
+++ TodoApp/TodoApp/FrmPrincipal.cs
@@ -14,9 +14,73 @@ namespace TodoApp
 {
     public partial class Form1 : Form
     {
+        List<Grupo> list;
         public Form1()
         {
             InitializeComponent();
+
+            Grupo grupo = new Grupo();
+            list = grupo.retrive();
+            InitializeNodeInTreeView(list);
+        }
+
+
+        private Grupo lookUpNodesChild(Grupo i)
+        {
+            foreach(Grupo it in this.list)
+            {
+                if(i.IdGrupo == it.IdGrupo)
+                {
+                    return it;
+                }
+            }
+            return null;
+        }
+
+
+        private void aggreateNodes(Grupo item, TreeNode parent)
+        {
+
+            if (item.Parent == -1)
+            {
+                TreeNode p = tvGrupos.Nodes.Add(item.Nombre);
+                if(item.childs != null)
+                {
+                    foreach(Grupo i in item.childs)
+                    {
+                        Grupo e = lookUpNodesChild(i);
+                        if (e != null)
+                            aggreateNodes(e, p);
+                    }
+                    
+                }
+            }
+            else
+            {
+                TreeNode pi = parent.Nodes.Add(item.Nombre);
+                if (item.childs != null)
+                {
+                    foreach (Grupo i in item.childs)
+                    {
+                        Grupo j = lookUpNodesChild(i);
+                        if (j != null)
+                            aggreateNodes(j, pi);
+                    }
+
+                }
+                // lookUpNodesChild();
+            }
+        }
+
+        private void InitializeNodeInTreeView(List<Grupo> list)
+        {
+            foreach (Grupo item in list)
+            {
+                if (item.Parent == -1)
+                {
+                   aggreateNodes(item, null);
+                }
+            }
         }
 
         private void btnAgregarNodo_Click(object sender, EventArgs e)
