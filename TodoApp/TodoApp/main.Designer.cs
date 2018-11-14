44ac96ca98a81d779181ebcfa3ae551f9f41075e
jicamachov <jicamachov@outlook.com>
2018-11-14 15:26:14 -0500

    Nueva Carga de grupos

--- TodoApp/TodoApp/main.Designer.cs
+++ TodoApp/TodoApp/main.Designer.cs
@@ -29,45 +29,67 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeViewGroup = new System.Windows.Forms.TreeView();
             this.add = new System.Windows.Forms.Button();
             this.del = new System.Windows.Forms.Button();
+            this.textGroup = new System.Windows.Forms.TextBox();
+            this.lblgroup = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.lblgroup);
+            this.panel1.Controls.Add(this.textGroup);
             this.panel1.Controls.Add(this.del);
             this.panel1.Controls.Add(this.add);
-            this.panel1.Controls.Add(this.treeView1);
+            this.panel1.Controls.Add(this.treeViewGroup);
             this.panel1.Location = new System.Drawing.Point(5, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(792, 432);
+            this.panel1.Size = new System.Drawing.Size(772, 432);
             this.panel1.TabIndex = 0;
             // 
-            // treeView1
+            // treeViewGroup
             // 
-            this.treeView1.Location = new System.Drawing.Point(7, 6);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(216, 361);
-            this.treeView1.TabIndex = 0;
-            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeViewGroup.Location = new System.Drawing.Point(7, 99);
+            this.treeViewGroup.Name = "treeViewGroup";
+            this.treeViewGroup.Size = new System.Drawing.Size(216, 330);
+            this.treeViewGroup.TabIndex = 0;
+            this.treeViewGroup.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
             // add
             // 
-            this.add.Location = new System.Drawing.Point(7, 373);
+            this.add.Image = global::TodoApp.Properties.Resources.if_plus_1646001;
+            this.add.Location = new System.Drawing.Point(49, 42);
             this.add.Name = "add";
-            this.add.Size = new System.Drawing.Size(96, 23);
+            this.add.Size = new System.Drawing.Size(77, 33);
             this.add.TabIndex = 1;
             this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
             // 
             // del
             // 
-            this.del.Location = new System.Drawing.Point(120, 373);
+            this.del.Location = new System.Drawing.Point(145, 42);
             this.del.Name = "del";
-            this.del.Size = new System.Drawing.Size(103, 23);
+            this.del.Size = new System.Drawing.Size(78, 33);
             this.del.TabIndex = 2;
             this.del.UseVisualStyleBackColor = true;
+            // 
+            // textGroup
+            // 
+            this.textGroup.Location = new System.Drawing.Point(49, 16);
+            this.textGroup.Name = "textGroup";
+            this.textGroup.Size = new System.Drawing.Size(174, 20);
+            this.textGroup.TabIndex = 3;
+            // 
+            // lblgroup
+            // 
+            this.lblgroup.AutoSize = true;
+            this.lblgroup.Location = new System.Drawing.Point(7, 19);
+            this.lblgroup.Name = "lblgroup";
+            this.lblgroup.Size = new System.Drawing.Size(36, 13);
+            this.lblgroup.TabIndex = 4;
+            this.lblgroup.Text = "Group";
             // 
             // main
             // 
@@ -78,6 +100,7 @@
             this.Name = "main";
             this.Text = "main";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -85,8 +108,10 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView treeViewGroup;
         private System.Windows.Forms.Button del;
         private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Label lblgroup;
+        private System.Windows.Forms.TextBox textGroup;
     }
 }
--- TodoApp/TodoApp/main.cs
+++ TodoApp/TodoApp/main.cs
@@ -7,19 +7,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TodoApp.Model;
 
 namespace TodoApp
 {
     public partial class main : Form
     {
+        List<Grupo> list;
         public main()
         {
             InitializeComponent();
+            Grupo grupo = new Grupo();
+            list = grupo.retrive();
+            InitializeNodeInTreeView(list);
         }
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
 
+        }
+
+
+
+        private Grupo lookUpNodesChild(Grupo i)
+        {
+            foreach (Grupo it in this.list)
+            {
+                if (i.IdGrupo == it.IdGrupo)
+                {
+                    return it;
+                }
+            }
+            return null;
+        }
+
+
+        private void aggreateNodes(Grupo item, TreeNode parent)
+        {
+
+            if (item.Parent == -1)
+            {
+                TreeNode p = treeViewGroup.Nodes.Add(item.Nombre);
+                if (item.childs != null)
+                {
+                    foreach (Grupo i in item.childs)
+                    {
+                        Grupo e = lookUpNodesChild(i);
+                        if (e != null)
+                            aggreateNodes(e, p);
+                    }
+
+                }
+            }
+            else
+            {
+                TreeNode pi = parent.Nodes.Add(item.Nombre);
+                if (item.childs != null)
+                {
+                    foreach (Grupo i in item.childs)
+                    {
+                        Grupo j = lookUpNodesChild(i);
+                        if (j != null)
+                            aggreateNodes(j, pi);
+                    }
+
+                }
+                // lookUpNodesChild();
+            }
+        }
+
+        private void InitializeNodeInTreeView(List<Grupo> list)
+        {
+            foreach (Grupo item in list)
+            {
+                if (item.Parent == -1)
+                {
+                    aggreateNodes(item, null);
+                }
+            }
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            //treeViewGroup
+
+            TreeNode nodoSeleccionado = this.treeViewGroup.SelectedNode;
+            TreeNode nodo = new TreeNode();
+            nodo.Text = this.textGroup.Text;
+               
+            if (nodoSeleccionado != null)
+            {
+                nodoSeleccionado.Nodes.Add(nodo);
+            }
+            else
+            {
+                //Raiz
+                this.treeViewGroup.Nodes.Add(nodo);
+            }
         }
     }
 }
